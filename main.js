381a6394788e3058d7cb2681c6fcdb8b26a593d7
Ruben Arcos <ruben.arcos06@gmail.com>
2023-07-27 14:29:54 -0500

    modified js, html, and css files.

--- main.js
+++ main.js
@@ -32,28 +32,62 @@ let alphabet = [
   "z",
 ];
 
-function randomWord() {
-  answer =
-    programming_languages[
-      Math.floor(Math.random(words) * programming_languages.length)
-    ];
+const wordDisplay = document.querySelector(".wordDisplay")
+const guessesText = document.querySelector(".incorrectGuess b")
+const keyboardDiv = document.querySelector(".keyboard");
+
+let currentWord;
+let correctLetters = [];
+let wrongGuessCount = 0;
+const maxGuesses = 6;
+
+const randomWord = () => {
+  const {word, hint} = words[Math.floor(Math.random() * words.length)]
+  currentWord = word;
+  console.log(word);
+  document.querySelector(".hintText b").innerText = hint;
+  wordDisplay.innerHTML = word.split("").map(() => `<li class="letter"></li>`).join("");
 }
 
-const btnClick = (event, letter) => {
-  console.log(event.target);
-  console.log("You just clicked on", letter);
-};
+const gameOver = (isVictory) => {
+  
+}
 
-alphabet.forEach((letter) => {
+const initGame = (button, clickedLetter) => {
+  if(currentWord.includes(clickedLetter)) {
+    [...currentWord].forEach((letter, index) => {
+      if(letter === clickedLetter) {
+        correctLetters.push(letter);
+        wordDisplay.querySelectorAll("li")[index].innerText = letter;
+        wordDisplay.querySelectorAll("li")[index].classList.add("guessed");
+      }
+    })
+  } else {
+    wrongGuessCount++;
+  }
+  button.disabled = true;
+  guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
+
+  if (wrongGuessCount === maxGuesses)
+  return gameOver(false);
+  if (correctLetters.length === currentWord.length)
+  return gameOver(true)
+}
+
+//creating keyboard buttons
+for (let i = 97; i <= 122; i++) {
   const button = document.createElement("button");
-  button.classList.add("letters");
-  button.setAttribute("type", "button");
-  button.addEventListener("click", (event) => {
-    btnClick(event, letter);
-  });
-  button.innerText = letter.toUpperCase();
-  keyboard.appendChild(button);
-});
+  button.innerText = String.fromCharCode(i);
+  keyboardDiv.appendChild(button);
+  button.addEventListener("click", e => initGame(e.target, String.fromCharCode(i)))
+}
+
+randomWord();
+
+
+
+
+
 
 let answer = "";
 
--- words.js
+++ words.js
@@ -1,32 +1,107 @@
-module.exports =  [ 
-    "JOCKEY",
-    "PROVISION",
-    "TRADITION",
-    "RAID",
-    "HILL",
-    "MEAN",
-    "SALON",
-    "STAFF",
-    "ILLUSION",
-    "SERVICE",
-    "SWIM",
-    "PANIC",
-    "LANGUAGE",
-    "RATE",
-    "MARINE",
-    "NEGATIVE",
-    "STATEMENT",
-    "STRIDE",
-    "PRINCE",
-    "ABOLISH",
-    "FAX",
-    "TICKET",
-    "DEPARTURE",
-    "JUST",
-    "INSIGHT",
-    "INCIDENT",
-    "FREIGHT",
-    "REQUEST",
-    "BURST",
-    "QUESTION",
-];
+// module.exports =  [ 
+//     "JOCKEY",
+//     "PROVISION",
+//     "TRADITION",
+//     "RAID",
+//     "HILL",
+//     "MEAN",
+//     "SALON",
+//     "STAFF",
+//     "ILLUSION",
+//     "SERVICE",
+//     "SWIM",
+//     "PANIC",
+//     "LANGUAGE",
+//     "RATE",
+//     "MARINE",
+//     "NEGATIVE",
+//     "STATEMENT",
+//     "STRIDE",
+//     "PRINCE",
+//     "ABOLISH",
+//     "FAX",
+//     "TICKET",
+//     "DEPARTURE",
+//     "JUST",
+//     "INSIGHT",
+//     "INCIDENT",
+//     "FREIGHT",
+//     "REQUEST",
+//     "BURST",
+//     "QUESTION",
+// ];
+
+const words = [
+    {
+        word: 'barcelona',
+        hint: 'La Sagrada Família, is located in this city'
+    },
+     {
+        word: 'kayak',
+        hint: 'Narrow watercraft'
+    },
+    {
+        word: 'pneumonia',
+        hint: 'infection that inflames the air sacs in one or both lungs'
+    },
+     {
+        word: 'illusion',
+        hint: 'something that deceives or misleads'
+    },
+     {
+        word: 'departure',
+        hint: 'going away'
+    },
+     {
+        word: 'tradition',
+        hint: 'customary pattern of thought'
+    },
+     {
+        word: 'pshaw',
+        hint: 'an expression of contempt or impatience.'
+    },
+     {
+        word: 'ivory',
+        hint: 'a hard white substance that is a variety of dentin'
+    },
+     {
+        word: 'lymph',
+        hint: 'a colorless fluid containing white blood cells'
+    },
+     {
+        word: 'awkward',
+        hint: 'hard to do or deal with'
+    },
+     {
+        word: 'boggle',
+        hint: 'to be surprised, confused, or alarmed'
+    },
+      {
+        word: 'jazz',
+        hint: 'the hardest word to guess according to science'
+    },
+     {
+        word: 'disavow',
+        hint: 'deny responsability or support for'
+    },
+     {
+        word: 'galaxy',
+        hint: 'a huge collection of gas, dust, and billions of stars'
+    },
+     {
+        word: 'affix',
+        hint: 'stick, attach, or fasten (something) to something else'
+    },
+     {
+        word: 'stretch',
+        hint: 'to become extended in length or breadth or both'
+    },
+     {
+        word: 'icebox',
+        hint: 'an insulated cabinet or chest with a partition for ice'
+    },
+     {
+        word: 'jackpot',
+        hint: 'a large amount of money that you win in a game or a lottery'
+    }
+]

6debcbd21124f25d01aefe009974bd3fdda420e3
Ruben Arcos <ruben.arcos06@gmail.com>
2023-07-27 15:42:50 -0500

    added comments in JS file and added more words to list

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ let correctLetters = [];
 let wrongGuessCount = 0;
 const maxGuesses = 6;
 
+//selecting a random word and hint from the words.js
 const randomWord = () => {
   const { word, hint } = words[Math.floor(Math.random() * words.length)];
   currentWord = word;
@@ -23,6 +24,7 @@ const randomWord = () => {
 const gameOver = (isVictory) => {};
 
 const initGame = (button, clickedLetter) => {
+  //checking if clickedLetter exists on the currentWord
   if (currentWord.includes(clickedLetter)) {
     [...currentWord].forEach((letter, index) => {
       if (letter === clickedLetter) {
@@ -32,6 +34,7 @@ const initGame = (button, clickedLetter) => {
       }
     });
   } else {
+    //if clicked letter doesn't exists then update the wrongGuessCount
     wrongGuessCount++;
   }
   button.disabled = true;
@@ -41,12 +44,17 @@ const initGame = (button, clickedLetter) => {
   if (correctLetters.length === currentWord.length) return gameOver(true);
 };
 
-//creating keyboard buttons
+//Creating keyboard buttons and attaching event listeners
+// For each lowercase letter (a to z)
 for (let i = 97; i <= 122; i++) {
+  // Create a new button element with the letter as its text
   const button = document.createElement("button");
   button.innerText = String.fromCharCode(i);
+  //Append the button to the keyboardDiv element
   keyboardDiv.appendChild(button);
+  // Add an event listener to the button to handle the click event
   button.addEventListener("click", (e) =>
+  //Call initGame function with the clicked button and the associated letter as parameters
     initGame(e.target, String.fromCharCode(i))
   );
 }
--- words.js
+++ words.js
@@ -1,4 +1,4 @@
-// module.exports =  [ 
+// module.exports =  [
 //     "JOCKEY",
 //     "PROVISION",
 //     "TRADITION",
@@ -32,76 +32,100 @@
 // ];
 
 const words = [
-    {
-        word: 'barcelona',
-        hint: 'La Sagrada Família, is located in this city'
-    },
-     {
-        word: 'kayak',
-        hint: 'Narrow watercraft'
-    },
-    {
-        word: 'pneumonia',
-        hint: 'infection that inflames the air sacs in one or both lungs'
-    },
-     {
-        word: 'illusion',
-        hint: 'something that deceives or misleads'
-    },
-     {
-        word: 'departure',
-        hint: 'going away'
-    },
-     {
-        word: 'tradition',
-        hint: 'customary pattern of thought'
-    },
-     {
-        word: 'pshaw',
-        hint: 'an expression of contempt or impatience.'
-    },
-     {
-        word: 'ivory',
-        hint: 'a hard white substance that is a variety of dentin'
-    },
-     {
-        word: 'lymph',
-        hint: 'a colorless fluid containing white blood cells'
-    },
-     {
-        word: 'awkward',
-        hint: 'hard to do or deal with'
-    },
-     {
-        word: 'boggle',
-        hint: 'to be surprised, confused, or alarmed'
-    },
-      {
-        word: 'jazz',
-        hint: 'the hardest word to guess according to science'
-    },
-     {
-        word: 'disavow',
-        hint: 'deny responsability or support for'
-    },
-     {
-        word: 'galaxy',
-        hint: 'a huge collection of gas, dust, and billions of stars'
-    },
-     {
-        word: 'affix',
-        hint: 'stick, attach, or fasten (something) to something else'
-    },
-     {
-        word: 'stretch',
-        hint: 'to become extended in length or breadth or both'
-    },
-     {
-        word: 'icebox',
-        hint: 'an insulated cabinet or chest with a partition for ice'
-    },
-     {
-        word: 'jackpot',
-        hint: 'a large amount of money that you win in a game or a lottery'
-    }
-]
+  {
+    word: "barcelona",
+    hint: "La Sagrada Família, is located in this city",
+  },
+  {
+    word: "kayak",
+    hint: "Narrow watercraft",
+  },
+  {
+    word: "pneumonia",
+    hint: "infection that inflames the air sacs in one or both lungs",
+  },
+  {
+    word: "illusion",
+    hint: "something that deceives or misleads",
+  },
+  {
+    word: "departure",
+    hint: "going away",
+  },
+  {
+    word: "tradition",
+    hint: "customary pattern of thought",
+  },
+  {
+    word: "pshaw",
+    hint: "an expression of contempt or impatience.",
+  },
+  {
+    word: "ivory",
+    hint: "a hard white substance that is a variety of dentin",
+  },
+  {
+    word: "lymph",
+    hint: "a colorless fluid containing white blood cells",
+  },
+  {
+    word: "awkward",
+    hint: "hard to do or deal with",
+  },
+  {
+    word: "boggle",
+    hint: "to be surprised, confused, or alarmed",
+  },
+  {
+    word: "jazz",
+    hint: "special kind of music characterized by improvization",
+  },
+  {
+    word: "disavow",
+    hint: "deny responsability or support for",
+  },
+  {
+    word: "galaxy",
+    hint: "a huge collection of gas, dust, and billions of stars",
+  },
+  {
+    word: "affix",
+    hint: "stick, attach, or fasten (something) to something else",
+  },
+  {
+    word: "stretch",
+    hint: "to become extended in length or breadth or both",
+  },
+  {
+    word: "icebox",
+    hint: "an insulated cabinet or chest with a partition for ice",
+  },
+  {
+    word: "jackpot",
+    hint: "a large amount of money that you win in a game or a lottery",
+  },
+  {
+    word: "libreville",
+    hint: "capitol city of Gabon located on the Komo River, near the Gulf of Guinea",
+  },
+  {
+    word: "cananite",
+    hint: "in this language scientists found the oldest written sentence found on a 3700 year old lice comb",
+  },
+  {
+    word: "tartessos",
+    hint: "historical civilization settled in the southern Iberian Peninsula characterized by its mixture of local Paleohispanic and Phoenician traits.",
+  },
+  {
+    word: "tlaloc",
+    hint: "is the god of rain in Aztec religion",
+  },
+  {
+    word: "springfield illinois",
+    hint: "U.S. state capital houses the Old State Capitol, where Abraham Lincoln delivered his “House Divided” address",
+  },
+  {
+    word: "ljubljana",
+    hint: "the capital of Slovenia",
+  },
+];
